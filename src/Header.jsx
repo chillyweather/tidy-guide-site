fec19798943ea596f7feee0770e086dc3a99c2af
Adir <slutzkis@gmail.com>
2023-12-11 11:01:45 +0200

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -28,7 +28,6 @@ const NavBar = ({ token, setToken }) => {
       <div class="logo-div">
         <img src={logoTidy} alt="Tidy Logo" onClick={handleHomeClick} />
       </div>
-      {/* <button onClick={handleHomeClick}>Home</button> */}
       {token && <button onClick={handleGuidesClick}>Browse Guides</button>}
       {token ? (
         <button onClick={handleLogoutClick}>Logout</button>
@@ -42,7 +41,6 @@ const NavBar = ({ token, setToken }) => {
 export const Header = ({ token, setToken }) => {
   return (
     <div className="header">
-      {/* <h1>Tidy Guide Web</h1> */}
       <NavBar token={token} setToken={setToken} />
     </div>
   );
