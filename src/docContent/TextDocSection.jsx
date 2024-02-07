859121331ef2411b04af759a7afb862a201a23e8
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-02-07 18:08:49 +0200

    Refactor TextDocSection to handle multiline text

--- src/docContent/TextDocSection.jsx
+++ src/docContent/TextDocSection.jsx
@@ -1,19 +1,26 @@
 import PropTypes from "prop-types";
 
-export const TextDocSection = ({ element, index }) => (
-  <div className={"section textSection"}>
-    <div className={"anchorLink"} id={element.title + index}></div>
-    {element.title && (
-      <div className={"title-row"}>
-        <h3 className={"title"}>{element.title}</h3>
-        <a href={"#top"} className={"back-link"}>
-          ⬆︎
-        </a>
-      </div>
-    )}
-    {element.text.length && <p>{element.text}</p>}
-  </div>
-);
+export const TextDocSection = ({ element, index }) => {
+  const textArray = element.text.split("\n");
+  return (
+    <div className={"section textSection"}>
+      <div className={"anchorLink"} id={element.title + index}></div>
+      {element.title && (
+        <div className={"title-row"}>
+          <h3 className={"title"}>{element.title}</h3>
+          <a href={"#top"} className={"back-link"}>
+            ⬆︎
+          </a>
+        </div>
+      )}
+      {/* {element.text.length && <p>{element.text}</p>} */}
+      {textArray.length &&
+        textArray.map(
+          (paragraph, i) => !!paragraph.length && <p key={i}>{paragraph}</p>
+        )}
+    </div>
+  );
+};
 
 TextDocSection.propTypes = {
   element: PropTypes.object,
