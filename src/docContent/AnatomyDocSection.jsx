388c44bbb96e25a130ccd57df501bd4f61972b8e
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-02-10 09:11:13 +0200

    ad image to anatomy

--- src/docContent/AnatomyDocSection.jsx
+++ src/docContent/AnatomyDocSection.jsx
@@ -1,21 +1,28 @@
 import PropTypes from "prop-types";
 
-export const AnatomyDocSection = ({ element, index }) => (
-  <div className={"section textSection"}>
-    <div className={"anchorLink"} id={element.title + index}></div>
-    {element.title && (
-      <div className={"title-row"}>
-        <h3 className={"title"}>{element.title}</h3>
-        <a href={"#top"} className={"back-link"}>
-          ⬆︎
-        </a>
+export const AnatomyDocSection = ({ element, index }) => {
+  console.log("element", element);
+  return (
+    <div className={"section textSection"}>
+      <div className={"anchorLink"} id={element.title + index}></div>
+      {element.title && (
+        <div className={"title-row"}>
+          <h3 className={"title"}>{element.title}</h3>
+          <a href={"#top"} className={"back-link"}>
+            ⬆︎
+          </a>
+        </div>
+      )}
+      <div className={"image-wrapper"}>
+        <img className={"image"} src={element.content.remoteImageLink} />
       </div>
-    )}
-    <div style={{ backgroundColor: "#fff", padding: "24px" }}>
-      <p style={{ padding: 0, margin: 0 }}>Coming soon...</p>
+
+      {/* <div style={{ backgroundColor: "#fff", padding: "24px" }}>
+        <p style={{ padding: 0, margin: 0 }}>Coming soon...</p>
+      </div> */}
     </div>
-  </div>
-);
+  );
+};
 
 AnatomyDocSection.propTypes = {
   element: PropTypes.object,
