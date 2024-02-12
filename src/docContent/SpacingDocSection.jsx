d4a09207f1e90770c76753fbbb15cc8f75a441a1
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2024-02-12 16:15:58 +0200

    Refactor SpacingDocSection component

--- src/docContent/SpacingDocSection.jsx
+++ src/docContent/SpacingDocSection.jsx
@@ -1,22 +1,28 @@
 import PropTypes from "prop-types";
-// import { ReactSVG } from "react-svg";
+import { ReactSVG } from "react-svg";
 
-export const SpacingDocSection = ({ element, index }) => (
-  <div className={"section textSection"}>
-    <div className={"anchorLink"} id={element.title + index}></div>
-    {element.title && (
-      <div className={"title-row"}>
-        <h3 className={"title"}>{element.title}</h3>
-        <a href={"#top"} className={"back-link"}>
-          ⬆︎
-        </a>
-      </div>
-    )}
-    <div style={{ backgroundColor: "#fff", padding: "24px" }}>
-      <p style={{ padding: 0, margin: 0 }}>Coming soon...</p>
+export const SpacingDocSection = ({ element, index }) => {
+  return (
+    <div className={"section textSection"}>
+      <div className={"anchorLink"} id={element.title + index}></div>
+      {element.title && (
+        <div className={"title-row"}>
+          <h3 className={"title"}>{element.title}</h3>
+          <a href={"#top"} className={"back-link"}>
+            ⬆︎
+          </a>
+        </div>
+      )}
+      {element.content.remoteImageLink && (
+        <ReactSVG
+          src={element.content.remoteImageLink}
+          style={{ width: "100%" }}
+          className="svg-wrapper"
+        />
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 SpacingDocSection.propTypes = {
   element: PropTypes.object,
