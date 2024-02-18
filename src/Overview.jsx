8bfe1b9cb3a2a94505528407a43a576ddc8c8679
Adir <slutzkis@gmail.com>
2024-02-18 16:50:00 +0200

    Update Overview.jsx

--- src/Overview.jsx
+++ src/Overview.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import fetchDocs from "./fetchDocs";
 import PropTypes from "prop-types";
 import { Guides } from "./Guides";
+import { IconLink } from "@tabler/icons-react";
 
 export const Overview = () => {
   const { data } = useQuery({
@@ -23,7 +24,20 @@ export const Overview = () => {
             <nav className={"navigation"}></nav>
           </div>
         </div>
-        <h1>Here be overview</h1>
+        <div className={"section headerSection"}>
+          <div className="title-wrapper">
+            <strong>
+              <h1 id={"sectionHeader"}>
+                Overview
+                <IconLink
+                  className={"copyLink"}
+                  onClick={() => navigator.clipboard.writeText(location.href)}
+                />
+              </h1>
+            </strong>
+            
+          </div>
+        </div>
         <section></section>
       </div>
     </div>
