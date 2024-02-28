4c95b6e568fbfea23926bf22853f40e2092c90e8
Adir <slutzkis@gmail.com>
2024-02-28 10:50:24 +0200

    updates

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,14 @@
+import "./Home.css";
+
 export const Home = () => {
   return (
     <div className="home">
-      <h2>Home</h2>
+
+      {/* <h2>Home</h2> */}
+      <section>
+        <h1>I love spending all day documenting my design decisions.<br/>
+          <b>Said no one. Ever.</b></h1>
+      </section>
     </div>
   );
 };
