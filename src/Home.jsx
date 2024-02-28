89df04d2321968b5a0cfc08b04fcae89114709a6
Adir <slutzkis@gmail.com>
2024-02-28 11:46:25 +0200

    updates

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,8 +6,12 @@ export const Home = () => {
 
       {/* <h2>Home</h2> */}
       <section>
-        <h1>I love spending all day documenting my design decisions.<br/>
+        <h1>I love spending all day documenting my design decisions.<br />
           <b>Said no one. Ever.</b></h1>
+        <h2>
+          <b>A new way to generate and discover documentation in Figma</b><br />
+          Meet Tidy — the intuitive documentation builder you’ve been looking for.
+        </h2>
       </section>
     </div>
   );
