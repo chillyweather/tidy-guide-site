744a12d7f5c64e262845de4aa7c1b1f5a7d7d3a2
Adir <slutzkis@gmail.com>
2024-04-08 10:44:49 +0300

    updates

--- src/Home-New.jsx
+++ src/Home-New.jsx
@@ -5,7 +5,7 @@ import BuilderIcon from "./assets/builder-icon.svg";
 import BuilderProduct from "./assets/video-builder.mp4";
 import ViewerIcon from "./assets/viewer-icon.svg";
 import ViewerProduct from "./assets/video-viewer.mp4";
-import Earlybirds from "./assets/early-birds.svg";
+import Earlybirds from "./assets/magic-hat.svg";
 import videoImage from "./assets/video-min.png";
 
 export const Home = () => {
@@ -75,40 +75,13 @@ export const Home = () => {
       </section>
 
       <section>
-        <p>
-          A new way to generate and discover documentation in Figma
+
+        <p className="hero-para">
+          <b>A new way to generate and discover documentation in Figma</b>
+          <br />
           Meet Tidy — the intuitive documentation builder you’ve been looking for.
         </p>
-        <button onClick={() => {window.open('https://www.figma.com/community/plugin/1346498731777233765/tidy-guide-beta')}}>Get early access to beta</button>
-      </section>
-
-      <section>
-        <img src={videoImage} alt="" />
-      </section>
-
-      <section className="flex-center">
-        <p id="animPara" className="size-64 spirit left-align max-600">
-          A new way to generate and<br />
-          discover documentation in Figma
-          <br /><br />
-          Meet Tidy Guide the intuitive<br />
-          documentation builder you've<br />
-          been looking for.
-          <br /><br />
-          Within minutes you will have<br />
-          comprehensive and up-to-date<br />
-          documentation of your design<br />
-          system.
-        </p>
-        <p id="animParaRef" className="size-64 spirit left-align max-600">
-          A new way to generate
-          and discover documentation
-          in Figma
-          <br /><br />
-          Meet Tidy Guide the intuitive documentation builder you've been looking for.
-          <br /><br />
-          Within minutes you will have comprehensive and up-to-date documentation of your design system.
-        </p>
+        <button onClick={() => { window.open('https://www.figma.com/community/plugin/1346498731777233765/tidy-guide-beta') }}>Get early access to beta</button>
       </section>
 
       <section>
@@ -124,81 +97,16 @@ export const Home = () => {
         </h2>
       </section>
 
-      <section>
-        <img src={BuilderIcon} className="product-icon" />
-        <h1 className="size-54">Meet Tidy Builder:</h1>
-        <h2>Don’t start from scratch, stand on the shoulders of giants 💪</h2>
-        <video
-          src={BuilderProduct}
-          className="big-image"
-          muted
-          loop
-          autoPlay
-          playsInline
-        />
-        <br />
-        <button className="product-button">Get started with Builder</button>
-      </section>
-
-      <section>
-        <img src={ViewerIcon} className="product-icon" />
-        <h1 className="size-54">Meet Tidy Viewer:</h1>
-        <h2>
-          Integrated documentation when needed. <br />
-          Wave goodbye to content switching 👋
-        </h2>
-        <video
-          src={ViewerProduct}
-          className="big-image"
-          muted
-          loop
-          autoPlay
-          playsInline
-        />
-        <br />
-        <button className="product-button">Get started with Viewer</button>
-      </section>
-
-      <section className="colored">
-        <div className="inner-colored">
-          <h1 className="size-72">How does Tidy work?</h1>
-          <h2>
-            Join teams worldwide who rely on Tidy to streamline their design
-            system <br />
-            documentation process and foster better collaboration!
-          </h2>
-          <iframe
-            className="emptyVideo"
-            src="https://www.youtube.com/embed/kCGNIeoPgY8?si=7V8XUZEfHvAi4FMD"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          ></iframe>
-          <div id="join"></div>
-        </div>
-      </section>
-
       <section className="colored">
         <div className="inner-colored">
           <footer>
             <img src={Earlybirds} />
             <div className="inner-footer">
-              <h1 className="size-54">Join the early birds</h1>
-              <p>
-                Join Tidy’s early access list now to revolutionize your design
-                process.
-                <br />
-                Go to Figma Community now and get started!
-              </p>
+              <h1 className="size-54">Your knowledge base across design teams</h1>
+              
             </div>
-            <button onClick={() => { window.open("https://www.figma.com/@wearekido", "_blank") }}>Join Tidy Guide Beta</button>
-            {/* <div className="product-input">
-              <form id="email-form" onSubmit={handleSubmit} onChange={handleChange}>
-                <input type="email" placeholder="Your email" id="user-email" />
-                <button type="submit" id="form-button">Join Beta</button>
-              </form>
-            </div> */}
+            <button onClick={() => { window.open("https://www.figma.com/community/plugin/1346498731777233765/tidy-guide-beta", "_blank") }}>Join Tidy Guide Beta</button>
+
             <p id="thanks">
               Thanks for joining Tidy Guide Beta! 🚀 <br />
               We'll review your details and notify you once approved.
