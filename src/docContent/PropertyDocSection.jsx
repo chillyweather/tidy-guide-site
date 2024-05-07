c43e768766e19f23833de92b20c41d060e819f01
Adir <slutzkis@gmail.com>
2024-05-07 16:19:16 +0300

    updates

--- src/docContent/PropertyDocSection.jsx
+++ src/docContent/PropertyDocSection.jsx
@@ -24,6 +24,18 @@ export const PropertyDocSection = ({ element, index }) => {
   );
 };
 
+function addToolTips(event) {
+  var x = event.target.getElementsByTagName("tspan");
+  var i;
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.includes("title")) {
+      var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'title');
+      newElement.innerHTML = x[i].innerHTML;
+      x[i].appendChild(newElement);
+    }
+  }
+}
+
 PropertyDocSection.propTypes = {
   element: PropTypes.object,
   index: PropTypes.number,
--- src/docContent/VariantsDocSection.jsx
+++ src/docContent/VariantsDocSection.jsx
@@ -3,7 +3,9 @@ import { ReactSVG } from "react-svg";
 
 export const VariantsDocSection = ({ element, index }) => {
   return (
-    <div className={"section textSection"}>
+    <div className={"section textSection variantsSection"}
+      onMouseOver={(event) => { addToolTips(event) }}
+    >
       <div className={"anchorLink"} id={element.title + index}></div>
       {element.title && (
         <div className={"title-row"}>
@@ -23,6 +25,18 @@ export const VariantsDocSection = ({ element, index }) => {
     </div>
   );
 };
+
+function addToolTips(event) {
+  var x = event.target.getElementsByTagName("tspan");
+  var i;
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.includes("title")) {
+      var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'title');
+      newElement.innerHTML = x[i].innerHTML;
+      x[i].appendChild(newElement);
+    }
+  }
+}
 
 VariantsDocSection.propTypes = {
   element: PropTypes.object,
