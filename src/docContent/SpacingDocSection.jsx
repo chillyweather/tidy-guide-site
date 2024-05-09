1f30e0e3fe3f22024c808f5240ef8349f5040cb1
Adir <slutzkis@gmail.com>
2024-05-09 15:37:02 +0300

    updates

--- src/docContent/SpacingDocSection.jsx
+++ src/docContent/SpacingDocSection.jsx
@@ -5,6 +5,7 @@ export const SpacingDocSection = ({ element, index }) => {
   return (
     <div className={"section textSection spacingsSection"}
       onMouseOver={(event) => { addToolTips(event) }}
+      onClick={() => { window.open(window.location.origin + "/image.html#" + element.content.remoteImageLink, "_blank") }}
     >
       <div className={"anchorLink"} id={element.title + index}></div>
       {element.title && (
